66a21f869f40211f1a83eb342e25bcc432efba84
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-03-12 21:41:19 +0100

    Lecture1: - Introduction - type of data - cycle for, while - work with string

---
+++ python_course/lecture1/main.py
@@ -0,0 +1,27 @@
+"""
+n = 5
+
+print(type(n))
+print(n)
+
+n = "123"
+
+print(type(n))
+print(n)
+
+"""
+
+a = 3
+b = "40"
+c = 6
+
+print("{} - {} - {}".format(a, b, c))
+
+print('Enter first number:')
+number = input()
+print(type(number))
+print(number)
+
+number = int(number)
+print(type(number))
+print(number)
