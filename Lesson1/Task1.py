f219a5510f3d37b457cb4a18a325a429f42990fa
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-03-13 00:08:11 +0100

    Task #1.
    Поработайте с переменными, создайте несколько,
    выведите на экран, запросите у пользователя несколько чисел и
    строк и сохраните в переменные, выведите на экран.

---
+++ Lesson1/Task1.py
@@ -0,0 +1,10 @@
+print("Enter your name:")
+name = input()
+
+print("Enter your password:")
+password = input()
+
+print("Enter your age:")
+age = int(input())
+
+print(f"Your data is: name - {name}, password - {password}, age - {age}")
