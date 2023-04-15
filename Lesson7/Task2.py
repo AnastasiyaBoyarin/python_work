98ee2feafb346b60862476792afcb7177f52a8b1
Anastasiya Boyarin <anastasiya.boyarin@mail.ru>
2023-04-15 14:13:15 +0200

    Lesson 7. Task 2.
    Made two variables weight, height as attribute on the level of the class.

--- Lesson7/Task2.py
+++ Lesson7/Task2.py
@@ -1,10 +1,10 @@
 class Road:
+    weight = 25
+    height = 5
 
     def __init__(self, length, width):
         self._length = length
         self._width = width
-        self.weight = 25
-        self.height = 5
 
     def mass(self):
         mass = self._length * self._width * self.weight * self.height / 1000
